14f5dc044ec1fa5a115334114ac4821ffbb4c0b3
joma74 <dev-mails@gmx.net>
2019-12-06 01:14:58 +0100

    chore: links are not readable

--- fibonacci-calc-frontend/src/App.js
+++ fibonacci-calc-frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from "react"
-import logo from "./logo.svg"
 import "./App.css"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import OtherPage from "./OtherPage"
@@ -10,12 +9,11 @@ function App() {
 		<Router>
 			<div className="App">
 				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
 					<h1>Welcome to the very distributed Fibonacci calculator</h1>
 					<Link to="/">Home</Link>
 					<Link to="/otherpage">Other Page</Link>
 				</header>
-				<div>
+				<div className="App-body">
 					<Route exact path="/" component={Fib}></Route>
 					<Route exact path="/otherpage" component={OtherPage}></Route>
 				</div>
