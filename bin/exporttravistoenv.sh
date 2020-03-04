d80aa8c9cfae2a6e241fd55eced8b190160eae0f
joma74 <dev-mails@gmx.net>
2020-03-04 01:40:01 +0100

    chore: helper script to seed env like travis

---
+++ bin/exporttravistoenv.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+####################################################
+# install http://mikefarah.github.io/yq/read/ first
+####################################################
+
+set -x
+
+# parses .travis.yml for content of
+# .. env:
+#      global:
+out=$(yq r .travis.yml env.global)
+# and replaces the leading dash with export
+out=$(echo "$out" | sed -E "s/^-/export/")
+
+# This whole is required cause bash -c can not resolve vars, neither on multiline
+while IFS= read -r line
+do
+   # ... nor on singleline.
+   # Used envsubst here...
+   thecmd=$(echo $line | envsubst)
+   # but had to use eval, cause a subshell ala 
+   # bash -c does not deliver
+   eval '$thecmd'
+done < <(printf '%s\n' "$out")
+
+set +x
