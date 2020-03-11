a2bb8cab66fe7324cfb89255961fb9eb39b57d26
joma74 <dev-mails@gmx.net>
2020-03-11 15:46:38 +0100

    chore(ci): Added record option to set image commands

--- bin/execkubectlcommands.sh
+++ bin/execkubectlcommands.sh
@@ -2,6 +2,6 @@
 
 kubectl apply -f k8s
 
-kubectl set image deployment/frontend-deployment frontend=${IMAGE_NAME_FRONTEND_PROD}
-kubectl set image deployment/server-deployment server=${IMAGE_NAME_SERVER_PROD}
-kubectl set image deployment/worker-deployment worker=${IMAGE_NAME_WORKER_PROD}
+kubectl set image deployment/frontend-deployment frontend=${IMAGE_NAME_FRONTEND_PROD} --record
+kubectl set image deployment/server-deployment server=${IMAGE_NAME_SERVER_PROD} --record
+kubectl set image deployment/worker-deployment worker=${IMAGE_NAME_WORKER_PROD} --record
