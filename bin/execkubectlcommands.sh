4f7f540f445a4ab3182280edd05523e49ee5f5ac
joma74 <dev-mails@gmx.net>
2020-03-11 15:45:12 +0100

    chore(ci): Activate kubectl commands

--- bin/execkubectlcommands.sh
+++ bin/execkubectlcommands.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-# kubectl apply -f k8s 
+kubectl apply -f k8s
+
+kubectl set image deployment/frontend-deployment frontend=${IMAGE_NAME_FRONTEND_PROD}
+kubectl set image deployment/server-deployment server=${IMAGE_NAME_SERVER_PROD}
+kubectl set image deployment/worker-deployment worker=${IMAGE_NAME_WORKER_PROD}
