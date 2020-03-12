59cd4396b74c0a6000877ba0e2ff474ea2aa31ef
joma74 <dev-mails@gmx.net>
2020-03-12 09:20:29 +0100

    chore(ci): Added trace activation to scripts

--- bin/buildimages_dev.sh
+++ bin/buildimages_dev.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -x
+
 docker build -t ${IMAGE_NAME_FRONTEND_DEV} -f ./fibonacci-calc-frontend/Dockerfile.dev ./fibonacci-calc-frontend
 docker build -t ${IMAGE_NAME_SERVER_DEV} -f ./fibonacci-calc-server/Dockerfile.dev ./fibonacci-calc-server
 docker build -t ${IMAGE_NAME_WORKER_DEV} -f ./fibonacci-calc-worker/Dockerfile.dev ./fibonacci-calc-worker
+
+set +x
--- bin/buildimages_prod.sh
+++ bin/buildimages_prod.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-docker build -t ${IMAGE_NAME_FRONTEND_PROD} -f ./fibonacci-calc-frontend/Dockerfile ./fibonacci-calc-frontend
-docker build -t ${IMAGE_NAME_SERVER_PROD} -f ./fibonacci-calc-server/Dockerfile ./fibonacci-calc-server
-docker build -t ${IMAGE_NAME_WORKER_PROD} -f ./fibonacci-calc-worker/Dockerfile ./fibonacci-calc-worker
+set -x
+
+docker build -t ${IMAGE_NAME_FRONTEND_PROD} -t ${IMAGE_NAME_FRONTEND_PROD}:$GIT_SHA -f ./fibonacci-calc-frontend/Dockerfile ./fibonacci-calc-frontend
+docker build -t ${IMAGE_NAME_SERVER_PROD} -t ${IMAGE_NAME_SERVER_PROD}:$GIT_SHA -f ./fibonacci-calc-server/Dockerfile ./fibonacci-calc-server
+docker build -t ${IMAGE_NAME_WORKER_PROD} -t ${IMAGE_NAME_WORKER_PROD}:$GIT_SHA -f ./fibonacci-calc-worker/Dockerfile ./fibonacci-calc-worker
+
+set +x
