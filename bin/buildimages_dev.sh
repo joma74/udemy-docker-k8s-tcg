68b8273ffc37806776d63cebcd2a6f973d7e485f
joma74 <dev-mails@gmx.net>
2020-03-14 22:38:17 +0100

    chore(ci): streamlined image tag names to fit minikube and gcp

--- bin/buildimages_dev.sh
+++ bin/buildimages_dev.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+docker image tag redis:latest ${IMAGE_NAME_REDIS_DEV}
+docker image tag postgres:latest ${IMAGE_NAME_POSTGRES_DEV}
 docker build -t ${IMAGE_NAME_FRONTEND_DEV} -f ./fibonacci-calc-frontend/Dockerfile.dev ./fibonacci-calc-frontend
 docker build -t ${IMAGE_NAME_SERVER_DEV} -f ./fibonacci-calc-server/Dockerfile.dev ./fibonacci-calc-server
 docker build -t ${IMAGE_NAME_WORKER_DEV} -f ./fibonacci-calc-worker/Dockerfile.dev ./fibonacci-calc-worker
--- bin/buildimages_prod.sh
+++ bin/buildimages_prod.sh
@@ -2,8 +2,12 @@
 
 set -x
 
-docker build -t ${IMAGE_NAME_FRONTEND_PROD} -t ${IMAGE_NAME_FRONTEND_PROD}:$GIT_SHA -f ./fibonacci-calc-frontend/Dockerfile ./fibonacci-calc-frontend
-docker build -t ${IMAGE_NAME_SERVER_PROD} -t ${IMAGE_NAME_SERVER_PROD}:$GIT_SHA -f ./fibonacci-calc-server/Dockerfile ./fibonacci-calc-server
-docker build -t ${IMAGE_NAME_WORKER_PROD} -t ${IMAGE_NAME_WORKER_PROD}:$GIT_SHA -f ./fibonacci-calc-worker/Dockerfile ./fibonacci-calc-worker
+docker image tag redis:latest ${IMAGE_NAME_REDIS_PROD}
+docker image tag redis:latest ${IMAGE_NAME_REDIS_GCP_PROD}
+docker image tag postgres:latest ${IMAGE_NAME_POSTGRES_PROD}
+docker image tag postgres:latest ${IMAGE_NAME_POSTGRES_GCP_PROD}
+docker build -t ${IMAGE_NAME_FRONTEND_PROD} -t ${IMAGE_NAME_FRONTEND_GCP_PROD} -t ${IMAGE_NAME_FRONTEND_GCP_PROD}:$GIT_SHA -f ./fibonacci-calc-frontend/Dockerfile ./fibonacci-calc-frontend
+docker build -t ${IMAGE_NAME_SERVER_PROD} -t ${IMAGE_NAME_SERVER_GCP_PROD} -t ${IMAGE_NAME_SERVER_GCP_PROD}:$GIT_SHA -f ./fibonacci-calc-server/Dockerfile ./fibonacci-calc-server
+docker build -t ${IMAGE_NAME_WORKER_PROD} -t ${IMAGE_NAME_WORKER_GCP_PROD} -t ${IMAGE_NAME_WORKER_GCP_PROD}:$GIT_SHA -f ./fibonacci-calc-worker/Dockerfile ./fibonacci-calc-worker
 
 set +x
