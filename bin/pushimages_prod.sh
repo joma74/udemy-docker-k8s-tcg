0146843c8f30e4c0dc998b4a0de7a9d47d610d43
joma74 <dev-mails@gmx.net>
2020-03-14 23:29:27 +0100

    chore(ci): fix gcp image name

--- bin/pushimages_prod.sh
+++ bin/pushimages_prod.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-docker push "${IMAGE_NAME_FRONTEND_PROD}"
-docker push "${IMAGE_NAME_SERVER_PROD}"
-docker push "${IMAGE_NAME_WORKER_PROD}"
+docker push "${${IMAGE_NAME_FRONTEND_GCP_PROD}}"
+docker push "${IMAGE_NAME_SERVER_GCP_PROD}"
+docker push "${IMAGE_NAME_WORKER_GCP_PROD}"
 
 set +x
