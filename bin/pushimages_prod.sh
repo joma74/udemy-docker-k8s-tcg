ff9b21ef75285aea366e850ca4fe8208322cf8a0
joma74 <dev-mails@gmx.net>
2020-03-22 23:37:11 +0100

    chore(ci): remove unintentional double subshell

--- bin/pushimages_prod.sh
+++ bin/pushimages_prod.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-docker push "${${IMAGE_NAME_FRONTEND_GCP_PROD}}"
+docker push "${IMAGE_NAME_FRONTEND_GCP_PROD}"
 docker push "${IMAGE_NAME_SERVER_GCP_PROD}"
 docker push "${IMAGE_NAME_WORKER_GCP_PROD}"
 
